dc2299399b14f7bba3eb788069685d5453581105
kylon <3252255+kylon@users.noreply.github.com>
2023-03-27 19:25:31 +0200

    update

--- custom_components/cloud/__init__.py
+++ custom_components/cloud/__init__.py
@@ -1,7 +1,17 @@
 """Cloud."""
+from __future__ import annotations
+
 DOMAIN = "cloud"
 
-def setup(hass, config):
-    
-    # Return boolean to indicate that initialization was successful.
-    return True
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.loader import bind_hass
+
+@bind_hass
+@callback
+def async_remote_ui_url(hass: HomeAssistant) -> str:
+    return ""
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    return False
